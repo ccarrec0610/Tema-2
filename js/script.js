18c42236a91b246c6651b0f7e8025454756ee5af
Celia Carmona Reche <ccarrec0610@g.educaand.es>
2025-01-24 11:18:33 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-           function pedirdatos() {
-            nombre=prompt("¿Cómo te llamas?")
-            edad=prompt("¿Qué edad tienes?")
-            alert (" Hola soy " +  nombre  + " y tengo " + edad + " años ")
-            let=nombre
-            let=edad                
-            }
-            
-        
