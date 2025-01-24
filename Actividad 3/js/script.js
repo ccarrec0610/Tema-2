8168d69aadc73b301cd2bd4fd5ea60205a752b42
Celia Carmona Reche <ccarrec0610@g.educaand.es>
2025-01-24 11:19:22 +0100

    Add files via upload

---
+++ Actividad 3/js/script.js
@@ -0,0 +1,9 @@
+           function pedirdatos() {
+            nombre=prompt("¿Cómo te llamas?")
+            edad=prompt("¿Qué edad tienes?")
+            alert (" Hola soy " +  nombre  + " y tengo " + edad + " años ")
+            let=nombre
+            let=edad                
+            }
+            
+        
