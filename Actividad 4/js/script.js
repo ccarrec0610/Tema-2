a65ac2a142ab878626341cf62471abc18d3e8d47
Celia Carmona Reche <ccarrec0610@g.educaand.es>
2025-01-30 21:48:14 +0100

    Update script.js

--- Actividad 4/js/script.js
+++ Actividad 4/js/script.js
@@ -1,7 +1,7 @@
            function calcular() {
             let a = parseInt (prompt ("dime el primer número"),10);
             let b = parseInt (prompt ("dime el segundo número)"), 10);
-            alert ("El primer número es: " + a + "\nEl segundo número es: " + b +"\nLa suma es: " + (a+b) +"\nLa resta es: " + (a-b) + "\nLa multiplicación es: " + (a*b) + "\nLa división es: "  + (a/b) + "\nEl resto es: " + (a%b) + "")
+            alert ("El primer número es: " + a + "\nEl segundo número es: " + b +"\nLa suma es: " + (a+b) +"\nLa resta es: " + (a-b) + "\nLa multiplicación es: " + (a*b) + "\nLa división es: "  + (a/b) + "\nEl resto es: " + (a%b) + "");
 
             
      
