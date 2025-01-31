fd2a9f1fcc2e5ecf2d2cec628219e2f334147a44
Celia Carmona Reche <ccarrec0610@g.educaand.es>
2025-01-31 11:51:20 +0100

    Add files via upload

---
+++ Actividad 5/js/script.js
@@ -0,0 +1,9 @@
+           function calcular() {
+            let numero1;
+            let numero2;
+            numero1 = parseInt(document.getElementById ("numero1").value);
+            numero2 = parseInt (document.getElementById("numero2").value);
+            document.getElementById("mensaje").innerHTML = ("El primer número es: " + numero1 + "<br>El segundo número es: " + numero2 +"<br>La suma es: " + (numero1+numero2) +"<br>La resta es: " + (numero1-numero2) + "<br>La multiplicación es: " + (numero1*numero2) + "<br>La división es: "  + (numero1%numero2) + "<br>El resto es: " + (numero1%numero2) + "");
+           }
+            
+        
