c73a0e00b2e1381f07a2a5c39258dae0013b5618
Celia Carmona Reche <ccarrec0610@g.educaand.es>
2025-02-07 11:09:18 +0100

    Add files via upload

---
+++ Actividad 6/js/script.js
@@ -0,0 +1,12 @@
+           function comprobar() {
+            let numero;
+            numero = parseInt(document.getElementById ("numero").value);
+
+            if (numero % 2 == 0 ) {
+                document.getElementById("mensaje").innerHTML = "El número es par";
+            }
+            else {
+                document.getElementById("mensaje").innerHTML = "El número es impar";
+                }
+            }
+
