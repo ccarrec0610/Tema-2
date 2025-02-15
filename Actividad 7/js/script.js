def10e8ddea3ca91495b4cba0a414f59718ece65
Celia Carmona Reche <ccarrec0610@g.educaand.es>
2025-02-15 18:47:38 +0100

    Update script.js

--- Actividad 7/js/script.js
+++ Actividad 7/js/script.js
@@ -1,13 +1,13 @@
            function comprobar() {
             let numero;
-            numero = parseInt(document.getElementById ("nota").value);
+            numero = parseFloat(document.getElementById("nota").value);
 
             if (numero < 5) {
                 document.getElementById("mensaje").innerHTML = "Suspenso";
             }
             else { 
                 document.getElementById("mensaje").innerHTML = "Aprobado";
-                if (numero > 10) {
+                if (numero > 10 && numero < 0) {
                     document.getElementById("mensaje").innerHTML = "La nota que has puesto no es válida";
                 }
         }
